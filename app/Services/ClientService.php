c7273b87858fae817e9a77353eb735d2b4b75f18
Fernando <fx2.sistemas@gmail.com>
2022-07-15 14:19:18 -0300

    service ok

---
+++ app/Services/ClientService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\ClientRepository;
+
+class ClientService
+{
+    protected ClientRepository $clientRepository;
+
+    public function __construct(ClientRepository $clientRepository)
+    {
+        $this->clientRepository = $clientRepository;
+    }
+
+    public function saveClient(string $name)
+    {
+        $client = $this->clientRepository->getByName($name);
+        if (!empty($client)) {
+            throw new \Exception("O cliente {$name} já está cadastrado no banco");
+        }
+
+        return $this->clientRepository->save(4, $name);
+    }
+
+    public function getClient($name = '')
+    {
+        if ($name == '') {
+            return $this->clientRepository->get();
+        }
+
+        return $this->clientRepository->getByName($name);
+    }
+}
--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 use App\Entities\Client;
 use App\Repositories\ClientRepository;
+use App\Services\ClientService;
 
 include_once 'vendor/autoload.php';
 
@@ -14,4 +15,9 @@ $clientRepository = new ClientRepository($client);
 
 //print_r($clientRepository->save(1, 'Fernando'));
 //print_r($clientRepository->get());
-print_r($clientRepository->getByName('Caio'));
+//print_r($clientRepository->getByName('Caio'));
+
+$clientService = new ClientService($clientRepository);
+
+//print_r($clientService->saveClient('Caio'));
+print_r($clientService->getClient('Fernando'));
